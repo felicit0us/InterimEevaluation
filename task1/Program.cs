ec6d7a1e59833766d5ec038a1f04957a484e53df
felicit0us <inc0.elite@yahoo.com>
2024-05-29 14:00:03 +0300

    added task solution

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,3 +2,44 @@
 // которая выведет все натуральные числа в промежутке от M до N. 
 // Использовать рекурсию, не использовать циклы.
 
+void PrintNaturalNumbers(int start, int end)
+{
+    if (start > end)
+    {
+        return;
+    }
+
+    Console.Write(start + " ");
+    PrintNaturalNumbers(start + 1, end);
+}
+
+int DetectMin(int a, int b)
+
+{
+    int min = a;
+    if (a > b)
+    {
+        min = b;
+    }
+    return min;
+}
+
+int DetectMax(int a, int b)
+
+{
+    int max = b;
+    if (a > b)
+    {
+        max = a;
+    }
+    return max;
+}
+
+Console.WriteLine("Введите значение M:");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение N:");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Натуральные числа в промежутке от {DetectMin(M, N)} до {DetectMax(M, N)}:");
+PrintNaturalNumbers(DetectMin(M, N), DetectMax(M, N));
