b7ecb737c03889915ac34bca893e55f6c8033bc1
felicit0us <inc0.elite@yahoo.com>
2024-05-29 19:28:07 +0300

    added task2 solution

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,3 +2,26 @@
 // Даны два неотрицательных числа m и n.
 
 
+Console.WriteLine("Введите значение n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение m:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"A({n}, {m}) = {Ackermann(n, m)}");
+
+int Ackermann(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if (m == 0)
+    {
+        return Ackermann(n - 1, 1);
+    }
+    else
+    {
+        return Ackermann(n - 1, Ackermann(n, m - 1));
+    }
+ }
