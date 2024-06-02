1f6382971c2781cca1de09ba7681f0d6de396f55
felicit0us <inc0.elite@yahoo.com>
2024-06-02 15:24:15 +0300

    added task3 solution

--- task3/Program.cs
+++ task3/Program.cs
@@ -2,3 +2,26 @@
 // начиная с конца. Использовать рекурсию, не использовать циклы.
 
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArrayfromlast(int indx, int[] array)
+{
+    if (indx == 0) return;
+    Console.Write($"{array[indx - 1]} ");
+    PrintArrayfromlast(indx - 1, array);
+}
+
+int[] arr = CreateArrayRndInt(10, 0, 99);
+Console.WriteLine();
+PrintArrayfromlast(arr.Length, arr);
